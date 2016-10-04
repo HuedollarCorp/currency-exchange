cb63abc90082d85a0194eb79a3c27b627257cddb
Danilo Freitas <dsurviver@gmail.com>
2016-10-04 11:46:31 -0300

    Currency from USD to BRL

--- currency@dsurviver.gmail.com/extension.js
+++ currency@dsurviver.gmail.com/extension.js
@@ -1,53 +1,94 @@
-
 const St = imports.gi.St;
+const Soup = imports.gi.Soup;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 
-let text, button;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+// const prefs = Me.imports.prefs
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
+const API_ADDRESS = 'http://api.fixer.io/';
+const LATEST_ENDPOINT = 'latest';
+const BASE_QUERY = 'base=';
+const SYMBOLS_QUERY = 'symbols=';
+
+let refreshButton, mainLayout, mainCurrencyText, iconBin;
+let soupSession;
+
+let lastCurrency = 0.00;
+
+function _createSoupSession() {
+    soupSession = new Soup.SessionAsync();
 }
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
+function _refreshCurrency() {
+    _createSoupSession();
+    var requestMessage = Soup.Message.new(
+        'GET', API_ADDRESS + LATEST_ENDPOINT + '?' + BASE_QUERY + 'USD');
+    soupSession.queue_message(requestMessage, function(session, message) {
+        if (message.status_code !== 200) { return; }
 
-    text.opacity = 255;
+        let body = JSON.parse(message.response_body.data);
+        let value = body.rates.BRL;
+        let arrow;
+        if (lastCurrency !== 0) {
+            if (value > lastCurrency) {
+                arrow = 'arrow-up-symbolic';
+            } else if (value < lastCurrency) {
+                arrow = 'arrow-down-symbolic';
+            }
+            let arrowIcon = new St.Icon({
+                icon_name: arrow,
+                style_class: 'system-status-icon'
+            });
+            iconBin.set_child(arrowIcon);
+        }
 
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+        lastCurrency = value;
+        mainCurrencyText.set_text('R$ ' + value.toString());
+    });
 }
 
-function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
+function init(extensionMeta) {;
+    let theme = imports.gi.Gtk.IconTheme.get_default();
+    theme.append_search_path(extensionMeta.path + "/icons");
 
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+    mainCurrencyText = new St.Label({ text: 'R$ 0.00' });
+    mainLayout = new St.BoxLayout();
+    refreshButton = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true
+    });
+    let refreshIcon = new St.Icon({
+        icon_name: 'refresh-symbolic',
+        style_class: 'system-status-icon'
+    });
+
+    refreshButton.set_child(refreshIcon);
+    refreshButton.connect('button-press-event', _refreshCurrency);
+    iconBin = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true
+    });
+
+    mainLayout.add(refreshButton, 0);
+    mainLayout.add(mainCurrencyText, 1);
+    mainLayout.add(iconBin, 2);
+
+    _refreshCurrency();
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    Main.panel._rightBox.insert_child_at_index(mainLayout, 0);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    Main.panel._rightBox.remove_child(mainLayout);
 }
